9dc1cb4748b4b8c4dd4cebfc63adc6e8556a1924
Daria Komzolova <d.komzolova@innopolis.university>
2026-05-14 18:59:32 +0300

    Initialize Spring Boot application

---
+++ src/main/java/com/example/sessionapp/SessionappApplication.java
@@ -0,0 +1,13 @@
+package com.example.sessionapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessionappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SessionappApplication.class, args);
+	}
+
+}
